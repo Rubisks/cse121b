e6b82ca6392600ca510486b5cdfadfaf2eb4bad4
Tuker Strand <strand.tucker@gmail.com>
2022-04-25 10:35:26 -0600

    in class activitys

---
+++ sandbox/week-2/js/main2.js
@@ -0,0 +1,78 @@
+// comarisons
+console.log(5>9);
+// === looks at the dat type so this is false
+console.log('5'===5)
+console.log(5>=5 && 6 == '6')
+// is five not equal to five causing this statement to be false
+console.log(5!=5 && 6 == '6')
+
+//setting a veriable
+let yourAge=13;
+
+if(yourAge >= 18){
+    console.log('you can vote');
+} else {
+    console.log('you cant vote');
+}
+
+// if statements
+let numSales = 1900;
+
+if (numSales <= 50) {
+    console.log("Way too few sales");
+  } else if (numSales <= 1000) {
+    console.log("Average number of sales");
+  } else {
+    console.log("A good number of sales.");
+  }
+  
+
+  //you can also use switches over if statements.
+let grade = "A";
+let gpaPoints = 0;
+switch (grade) {
+    case "A":
+        gpaPoints = 4;
+        break;
+    case "B":
+        gpaPoints = 3;
+        break;
+    case "C":
+        gpaPoints = 2;
+        break;
+    case "D":
+        gpaPoints = 1;
+        break;
+    case "F":
+        gpaPoints = 0;
+        break;
+      default:
+        gpaPoints = null;
+}
+
+console.log(gpaPoints);
+
+// looking at a range
+
+yourAge = 4;
+if(yourAge < 5){
+    console.log('not in school or preschool')
+} else if(yourAge <= 18 ){
+    console.log('you are in secondary school')
+} else{
+    console.log('you could be in collage')
+}
+
+// how to get a random bumber
+// creating a function with java
+
+let num =Math.random();
+function flip(){
+    if(num > .5){
+        console.log('Heads')
+    } else{
+        console.log('tails')
+    }
+}
+
+
