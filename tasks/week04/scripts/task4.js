bfcda66b60bf9af7d66f8644f37eaa541e59ee8f
Tuker Strand <strand.tucker@gmail.com>
2022-05-10 20:41:59 -0600

    week four task

--- tasks/week04/scripts/task4.js
+++ tasks/week04/scripts/task4.js
@@ -36,21 +36,35 @@ myInfo.placesLived = [
 /* OUTPUT */
 
 // Step 1: Assign the value of the name property (of the object declared above) to the HTML <span> element with an ID of name
-
+document.querySelector('#name').textContent = myInfo.name;
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
-
+document.querySelector('#photo').src = 'images/photo.jpeg';
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
-
+document.querySelector('#photo').setAttribute('alt', 'photo of Tucker');
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
-
-// Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
-
+const foodList = document.getElementById('favorite-foods');
+let Foods = '';
+myInfo.favoriteFoods.forEach(item =>{
+    Foods += "<li>" + item + "</li>"; 
+})
+// Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foo
+foodList.innerHTML = Foods;
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
-
+const hobbieList = document.getElementById('hobbies');
+let myhobbies = '';
+myInfo.hobbies.forEach(item =>{
+    myhobbies += "<li>" + item + "</li>";
+})
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
+hobbieList.innerHTML = myhobbies;
 
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
 // - Create an HTML <dd> element and put its length property in the <dd> element
-
+let locationsLived = ''
+const locationList = document.getElementById('places-lived');
+myInfo.placesLived.forEach(location =>{
+    locationsLived += "<dt>" + location.place + "</dt>" + "<dd>" + location.length + "</dd>";
+})
+locationList.innerHTML = locationsLived ;
 // Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-lived
